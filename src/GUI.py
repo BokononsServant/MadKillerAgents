b3ac3152dddc638a4695a13b1c4b1e0b71f238f7
Sepp <gabriel.kirchgraber@gmail.com>
2016-11-12 22:29:58 +0100

    x

--- src/GUI.py
+++ src/GUI.py
@@ -1,33 +1,87 @@
 from Tkinter import *
 from docutils.nodes import compound
+from pygments.styles.paraiso_dark import BACKGROUND
+import random
+import time
 
 class MyApp:
     def __init__(self, parent):
-        
+       
         self.myParent = parent
         self.myContainer1 = Frame(parent)
-        self.myContainer1.pack()  
-    
+        self.myContainer1.pack()   
+                
+        #self.photo=PhotoImage(file="./Tileset/baum.gif")
         
-        self.photo=PhotoImage(file="./Tileset/baum.gif")
+        #self.L1 = Label(self.myContainer1,image=self.photo, text="za", compound=CENTER, fg="green", bg="white",font = "Helvetica 16 bold italic")       
+        #self.L1.pack()     
         
-        self.L1 = Label(self.myContainer1,image=self.photo, text="za", compound=CENTER, fg="green", bg="white",font = "Helvetica 16 bold italic")       
-        self.L1.pack()     
+        #self.L2 = Label(self.myContainer1,image=self.photo, text="zu", compound=CENTER, fg="green", bg="white",font = "Helvetica 16 bold italic")       
+        #self.L2.pack()
         
-        self.L2 = Label(self.myContainer1,image=self.photo, text="zu", compound=CENTER, fg="green", bg="white",font = "Helvetica 16 bold italic")       
-        self.L2.pack()
+        self.x=40
+        self.y =40
         
-        self.x=30
-        self.y =20
+        #self.L3 = Label(self.myContainer1,bg="green",height=self.y, width=self.x, text=str(self.x)+" x "+str(self.y))
+        #self.L3.pack()
         
-        self.L3 = Label(self.myContainer1,bg="green",height=self.y, width=self.x, text=str(self.x)+" x "+str(self.y))
-        self.L3.pack()
+        self.p=PhotoImage()
+        self.dimX=20
+        self.dimY=20
+        #self.L1 = Label(self.myContainer1,bg="red",relief='solid',image=self.p,height=self.y, compound='center',width=self.x, text='sex')
+        #self.L1.pack(side=RIGHT)
+        self.k=0
+        self.map=[[0 for i in range(self.dimY)] for i in range(self.dimX)]
         
-        self.L4 = Label(self.myContainer1,bg="red",height=self.y, width=self.x, text=str(self.x)+" x "+str(self.y))
-        self.L4.pack(side=RIGHT)
+        for i in range(self.dimX):
+            for j in range(self.dimY):
+                self.map[i][j]=(Label(self.myContainer1,bg="red",relief='solid',image=self.p,height=self.y, compound='left',width=self.x,text=random.randint(1,6)))
+                self.map[i][j].grid(row=self.dimY-j,column=i)          
+                #self.tiles[self.i,j].grid(row=self.j,column=self.i)
+                
+        #self.L1['text']="green"
+        #self.["background"]='green'
+        #self.tiles[1].config(text = "sdf")
+        #print len(self.tiles)
+        #print self.tiles[1]
+        self.moves=[[-1,0],[1,0],[0,1],[0,-1]]
+        self.pos=[random.randint(0,self.dimX-1),random.randint(0,self.dimY-1)]                
+        self.map[self.pos[0]][self.pos[1]].configure(bg="green")   
         
-        
+        print self.pos         
+     
+        for i in range(100):         
+            self.myContainer1.after(i*400,self.RandomMove)  
+            #self.myContainer1.after(200,self.RandomMove)
+            
+            #self.delay
+            
 
+    def RandomMove(self):
+        
+        self.map[self.pos[0]][self.pos[1]].configure(bg="red")
+            
+        self.move=random.choice(self.moves)            
+        self.moveX=self.move[0]
+        self.moveY=self.move[1]
+            
+        while self.moveX+self.pos[0]<0 or self.moveX+self.pos[0]>self.dimX-1 or self.moveY+self.pos[1]<0 or self.moveY+self.pos[1]>self.dimY-1:
+            self.move=random.choice(self.moves)            
+            self.moveX=self.move[0]
+            self.moveY=self.move[1]
+                
+        self.map[self.pos[0]+self.moveX][self.pos[1]+self.moveY].configure(bg="green")
+            
+        self.pos=[self.pos[0]+self.moveX,self.pos[1]+self.moveY]
+        
+        #print self.pos            
+        
+         
+                    
+
+        #self.map[4][0].configure(bg="green")
+        #self.map[4-1][0+1].configure(bg="green")
+        
 
 root = Tk()
 myapp = MyApp(root)
--- src/Test.py
+++ src/Test.py
@@ -1,35 +1,6 @@
-from Tkinter import *
 
-class MyApp:
-    def __init__(self, parent):
-        self.myParent = parent
-        self.myContainer1 = Frame(parent)
-        self.myContainer1.pack()
+moves1=[[-1,0],[1,0],[0,1],[0,-1]]
 
-        self.button1 = Button(self.myContainer1, command=self.button1Click) ### (1)
-        self.button1.configure(text="OK", background= "green")
-        self.button1.pack(side=LEFT)
-        self.button1.focus_force()
+moves2=[[3,3],[4,4]]
 
-
-        self.button2 = Button(self.myContainer1, command=self.button2Click)  ### (2)
-        self.button2.configure(text="Cancel", background="red")
-        self.button2.pack(side=RIGHT)
-
-
-    def button1Click(self):  ### (3)
-        print "button1Click event handler"
-        if self.button1["background"] == "green":
-            self.button1["background"] = "yellow"
-        else:
-            self.button1["background"] = "green"
-
-    def button2Click(self): ### (4)
-        print "button2Click event handler"
-        self.myParent.destroy()
-
-
-
-root = Tk()
-myapp = MyApp(root)
-root.mainloop()
+print moves1[0][0]+moves2[1][0]
