666d21b8b4aec73f631b3c6d34bf37eeb05f82e6
Sepp <gabriel.kirchgraber@gmail.com>
2016-12-08 13:37:06 +0100

    working on interface: click to move works

--- src/TileSelection.py
+++ src/TileSelection.py
@@ -3,33 +3,38 @@ prev_selection= None
 class TileSelection():
     
     def __init__(self,clicked_widget,MAO):
-        global prev_selection
         self.MAO=MAO
         for x in range(self.MAO.dimX):
             for y in range(self.MAO.dimY):
                 if self.MAO.map1.map[x][y].label == clicked_widget:
-                    if prev_selection == None: 
-                        prev_selection = self.MAO.map1.map[x][y]
-                        print "Selected %s %s !"%(self.MAO.map1.map[x][y].x,self.MAO.map1.map[x][y].y)
-                    elif prev_selection == self.MAO.map1.map[x][y]: 
-                        prev_selection = None
-                        print "Nothing selected!"
-                    elif prev_selection.army == None: 
-                        prev_selection=self.MAO.map1.map[x][y]
-                        print "Selected %s %s"%(self.MAO.map1.map[x][y].x,self.MAO.map1.map[x][y].y)
                     self.tile_selection(self.MAO.map1.map[x][y])
 
     def tile_selection(self,Tile):
         global prev_selection
-        if Tile.army == None:
+        
+        if prev_selection == Tile:
             prev_selection = None
-            return
-        elif Tile.army.owner !=self.MAO.active_player:
-            prev_selection = None
-            return
-        else:
-            if prev_selection in Tile.surrounding_tiles:
+            print "Nothing selected!"
+        
+        elif prev_selection == None and Tile.owner == self.MAO.active_player and Tile.army != None:
+            prev_selection = Tile
+            print "Tile %s %s selected!"%(Tile.x, Tile.y)
+
+        elif prev_selection != None and prev_selection.army != None and prev_selection.owner == self.MAO.active_player:
+            
+            if Tile in prev_selection.surrounding_tiles:
                 prev_selection.army.move(Tile)
+                prev_selection = None
+            else:
+                print "Not enough movement points!"
+                
+
+        
+
+            
+        
+            
+            
                 
         
         
